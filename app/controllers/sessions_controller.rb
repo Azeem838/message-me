ee85cabf24d7e901382e2103b2be4178fdfe4f93
Azeem <azeemahmed101@gmail.com>
2020-06-20 13:27:39 +0200

    add controller restrictions if logged_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: %i[new create]
+
   def new; end
 
   def create
     user = User.find_by(username: params[:session][:username])
     if user&.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in"
+      flash[:success] = 'You have successfully logged in'
       redirect_to root_path
     else
       flash.now[:error] = 'There was something wrong with your log in information'
@@ -17,7 +19,16 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "You have successfully logged out"
+    flash[:success] = 'You have successfully logged out'
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = 'You are already logged in'
+      redirect_to root_path
+    end
   end
 end
