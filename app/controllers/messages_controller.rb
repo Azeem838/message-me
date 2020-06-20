a5b89a1c952151958c602d73bcffa03d82021384
Azeem <azeemahmed101@gmail.com>
2020-06-20 15:01:32 +0200

    add real-time message function with ActionCable through websocket

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,12 +5,16 @@ class MessagesController < ApplicationController
 
   def create
     message = current_user.messages.build(message_params)
-    redirect_to root_path if message.save
+    ActionCable.server.broadcast 'chatroom_channel', mod_message: message_render(message) if message.save
   end
 
   private
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: { message: message })
   end
 end
