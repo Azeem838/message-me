b2d0758c6744a48e2902d79f6de2fdfd97185b38
Azeem <azeemahmed101@gmail.com>
2020-06-20 13:14:47 +0200

    add authentication to log in and log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
 class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user&.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "You have successfully logged in"
+      redirect_to root_path
+    else
+      flash.now[:error] = 'There was something wrong with your log in information'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
+  end
 end
