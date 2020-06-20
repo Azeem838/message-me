cefb23b164557a46f929bba1e7320b5aa9b87259
Azeem <azeemahmed101@gmail.com>
2020-06-20 12:09:01 +0200

    add message model and db

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@
 
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 15 }
+  has_many :messages
   has_secure_password
 end
